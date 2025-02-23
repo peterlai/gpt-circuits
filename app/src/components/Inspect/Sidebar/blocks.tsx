22e3b0a74e682bf93781195dba4d8bd8b879845d
Peter Lai <peterjlai@gmail.com>
2025-02-22 23:25:40 -0800

    adding placeholder samples section

--- app/src/components/Inspect/Sidebar/blocks.tsx
+++ app/src/components/Inspect/Sidebar/blocks.tsx
@@ -1,13 +1,18 @@
-import { useAtomValue } from "jotai";
+import { useAtom, useAtomValue } from "jotai";
 import { FaLayerGroup } from "react-icons/fa6";
 import { BlockData } from "../../../stores/Block";
 import { printableTokensAtom, targetIdxAtom } from "../../../stores/Graph";
+import { predictionDataAtom } from "../../../stores/Prediction";
 import { CloseButton } from "./close";
+import { ErrorMessage, LoadingMessage } from "./loading";
+import { SearchableSamples } from "./samples";
 
 function BlockSidebar({ block }: { block: BlockData }) {
   return (
     <>
       <BlockHeader block={block} />
+      <BlockProfile block={block} />
+      <BlockSamplesSection block={block} />
     </>
   );
 }
@@ -20,9 +25,7 @@ function BlockHeader({ block }: { block: BlockData }) {
   return (
     <header>
       <span className="feature-location">
-        <span>Examples like</span>&nbsp;
-        <pre className="token">{blockToken}</pre>&nbsp;
-        <span>on</span>
+        <pre className="token">{blockToken}</pre>
         <span className="layer">
           <FaLayerGroup className="icon" />
           {block.layerIdx > 0 ? `${block.layerIdx}` : "Embedding"}
@@ -30,6 +33,54 @@ function BlockHeader({ block }: { block: BlockData }) {
       </span>
       <CloseButton />
     </header>
+  );
+}
+
+function BlockProfile({ block }: { block: BlockData }) {
+  return (
+    <section className="block-profile">
+      <h3>Features</h3>
+      <table className="charts-css bar multiple stacked show-labels data-spacing-1">
+        <tbody>
+          {Object.values(block.features).map((feature) => {
+            return (
+              <tr key={feature.featureId}>
+                <th scope="row">
+                  <span>#{feature.featureId}</span>
+                </th>
+                <td style={{ "--size": feature.normalizedActivation } as React.CSSProperties}>
+                  <span className="data"> {feature.activation.toFixed(2)}</span>
+                </td>
+                <td
+                  style={{ "--size": 1.0 - feature.normalizedActivation } as React.CSSProperties}
+                ></td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </section>
+  );
+}
+
+function BlockSamplesTitle() {
+  return <span>Similar Examples</span>;
+}
+
+function BlockSamplesSection({ block }: { block: BlockData }) {
+  // TODO: Switch to block-specific samples
+  const [{ data: predictionData, isPending, isError }] = useAtom(predictionDataAtom);
+
+  if (isPending) {
+    return <LoadingMessage />;
+  }
+
+  if (isError || !predictionData) {
+    return <ErrorMessage />;
+  }
+
+  return (
+    <SearchableSamples samples={predictionData.samples} titleComponent={<BlockSamplesTitle />} />
   );
 }
 
