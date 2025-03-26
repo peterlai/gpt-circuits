43e036abf9cde4dbf5f1e93e10422a63daef0ee8
Peter Lai <peterjlai@gmail.com>
2025-03-26 16:00:12 -0700

    fixing config

--- config/gpt/training.py
+++ config/gpt/training.py
@@ -54,7 +54,7 @@ options: dict[str, GPTTrainingConfig] = map_options(
     GPTTrainingConfig(
         name="stories_256x4",
         gpt_config=gpt_options["tiktoken_256x4"],
-        data_dir="data/simple_stories_10m",
+        data_dir="data/simple_stories",
         eval_interval=250,
         eval_steps=100,
         batch_size=512,
