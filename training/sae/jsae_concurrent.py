50ee74f16498a7b28fe5a3b6d873b915fe13d57a
David Quarel <davidquarel@gmail.com>
2025-04-23 17:21:23 +0000

    fixed jsae bug where only 4 out of 8 layers was being saved

--- training/sae/jsae_concurrent.py
+++ training/sae/jsae_concurrent.py
@@ -19,6 +19,11 @@ from models.sparsified import SparsifiedGPTOutput
 from training.sae import SAETrainer
 from training.sae.concurrent import ConcurrentTrainer
 
+import dataclasses
+import json
+from config.sae.models import SAEConfig
+
+from typing import Optional
 # Change current working directory to parent
 # while not os.getcwd().endswith("gpt-circuits"):
 #     os.chdir("..")
@@ -60,6 +65,7 @@ def parse_args() -> argparse.Namespace:
     parser.add_argument("--load_from", type=str, help="GPT model weights to load", default="shakespeare_64x4")
     parser.add_argument("--name", type=str, help="Model name for checkpoints")
     parser.add_argument("--sparsity", type=float, help="Jacobian sparsity loss coefficient", default=0.02)
+    parser.add_argument("--max_steps", type=int, help="Maximum number of steps to train", default=7500)
     return parser.parse_args()
 
 
@@ -89,9 +95,45 @@ class JSaeTrainer(ConcurrentTrainer):
         if self.ddp:
             # HACK: We're doing something that causes DDP to crash unless DDP optimization is disabled.
             torch._dynamo.config.optimize_ddp = False  # type: ignore
+            
+    def save_checkpoint(self, model: JSparsifiedGPT, is_best: torch.Tensor):
+        """
+        Save SAE weights for layers that have achieved a better validation loss.
+        """
+        # As weights are shared, only save if each layer is better than the previous best.
+        dir = self.config.out_dir
+        
+        self.model.gpt.save(dir)
+
+        # Save SAE config
+        meta_path = os.path.join(dir, "sae.json")
+        meta = dataclasses.asdict(self.config.sae_config, dict_factory=SAEConfig.dict_factory)
+        with open(meta_path, "w") as f:
+            json.dump(meta, f)
+        for layer_idx in self.model.layer_idxs:
+            if is_best[layer_idx]:
+            # save this pair of saes!
+                for loc in ['mlpin', 'mlpout']:
+                    sae = self.model.saes[f'{layer_idx}_{loc}']
+                    assert "jsae" in sae.config.sae_variant
+                    sae.save(Path(dir))
+
+    def calculate_loss(self, x, y, is_eval) -> tuple[torch.Tensor, Optional[dict[str, torch.Tensor]]]:
+        """
+        Calculate model loss.
+        """
+        output: SparsifiedGPTOutput = self.model(x, y, is_eval=is_eval)
+        loss = self.output_to_loss(output, is_eval=is_eval)
+        metrics = None
+
+        # Only include metrics if in evaluation mode
+        if is_eval:
+            metrics = self.gather_metrics(loss, output)
+
+        return loss, metrics
 
     # TODO: This is a very expensive operation, we should try to speed it up
-    def output_to_loss(self, output: SparsifiedGPTOutput) -> torch.Tensor:
+    def output_to_loss(self, output: SparsifiedGPTOutput, is_eval: bool= False) -> torch.Tensor:
         """
         Return an array of losses, one for each pair of layers:
         loss[i] = loss_recon[f"{i}_mlpin"] + loss_recon[f"{i}_mlpout"] 
@@ -99,10 +141,13 @@ class JSaeTrainer(ConcurrentTrainer):
         """
         device = output.sae_losses.device
         recon_losses = output.sae_losses
-        jacobian_losses = torch.empty(len(self.model.layer_idxs), device=device)
+        jacobian_losses = torch.zeros(len(self.model.layer_idxs), device=device)
         j_coeffs = torch.tensor(self.model.loss_coefficients.sparsity, device=device)
         
+        
         for layer_idx in self.model.layer_idxs:
+            if self.model.loss_coefficients.sparsity[layer_idx] == 0 and not is_eval: # compute jacobian loss only on eval if sparsity is 0
+                continue
             topk_indices_mlpin = output.indices[f'{layer_idx}_mlpin']
             topk_indices_mlpout = output.indices[f'{layer_idx}_mlpout']
             
@@ -150,8 +195,10 @@ if __name__ == "__main__":
 
     if args.name:
         config.name = args.name
+    if args.max_steps:
+        config.max_steps = args.max_steps
         
-    if args.sparsity:
+    if args.sparsity is not None:
         num_trainable_layers = len(config.loss_coefficients.sparsity)
         config.loss_coefficients.sparsity = (args.sparsity, ) * num_trainable_layers
         print(f"Setting sparsity to {args.sparsity} for all {num_trainable_layers} layers")
@@ -163,6 +210,8 @@ if __name__ == "__main__":
 
     # Initialize trainer
     trainer = JSaeTrainer(config, load_from=TrainingConfig.checkpoints_dir / args.load_from)
+    print(f'{trainer.model.saes.keys()=}')
+    print(f'{trainer.model.layer_idxs=}')
     trainer.train()
 
 # %%
